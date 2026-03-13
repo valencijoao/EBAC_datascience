b496468e1dfb1ba73f2e46fd243fd52e30cabb2a
João Valenci <valencijoao30@gmail.com>
2026-03-13 14:51:22 -0300

    Resolução do Exercício 5 incluída

--- lab8.py
+++ lab8.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df = pd.read_csv('/data/ecommerce_ex3.csv')
+df = pd.read_csv('/data/ecommerce_ex4.csv')
 df = df.drop_duplicates()
 df = df.dropna(thresh=8)
 
@@ -55,6 +55,26 @@ limite_alto = q3 + 1.5 * iqr
 
 # Filtrar os produtos que possuem um número de avaliações maior que o limite superior
 df_avaliados = df[df['N_Avaliacoes'] > limite_alto]
+
+# Exercício 4:
+
+# Escreva seu código abaixo
+
+# Extrair e limpar os dados da coluna 'Condicao'
+# A função lambda é usada aqui para pegar a primeira palavra da string na coluna 'Condicao'
+# x.split(' ')[0] pega a primeira palavra da string.
+df['Condicao_Atual'] = df['Condicao'].apply(lambda x: x.split(' ')[0])
+
+# A função lambda é usada aqui para pegar a quinta palavra da string na coluna 'Condicao' se existir,
+# caso contrário, retorna 'Nenhum'
+df['Qtd_Vendidos'] = df['Condicao'].apply(lambda x: x.split(' ')[4] if len(x.split(' ')) > 5 else 'Nenhum')
+
+# Converter a coluna 'Desconto' para string
+df['Desconto'] = df['Desconto'].astype(str)
+
+# A função lambda é usada aqui para remover o símbolo '%' da string na coluna 'Desconto'
+df['Desconto'] = df['Desconto'].apply(lambda x: str(x).split('%')[0])
+
 
 
 
