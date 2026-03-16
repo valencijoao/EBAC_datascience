b2346c583392d9e238e4bd64a9d56d71acd9ed12
João Valenci <valencijoao30@gmail.com>
2026-03-16 18:30:03 -0300

    Conclusão do lab 9

--- lab9.py
+++ lab9.py
@@ -22,7 +22,7 @@ df = df.drop(['reais','centavos','condicao','condicao_atual'], axis= 1)
 
 scaler = MinMaxScaler()
 df['Nota_MinMax'] = scaler.fit_transform(df[['Nota']])
-df['N_Avaliações_MinMax'] = scaler.fit_transform(df[['N_Avaliações']])
+df['N_Avaliacoes_MinMax'] = scaler.fit_transform(df[['N_Avaliacoes']])
 df['Desconto_MinMax'] = scaler.fit_transform(df[['Desconto']])
 df['Preco_MinMax'] = scaler.fit_transform(df[['Preco']])
 
@@ -30,3 +30,26 @@ encoder = LabelEncoder()
 df['Marca_Cod'] = encoder.fit_transform(df[['Marca']])
 df['Material_Cod'] = encoder.fit_transform(df[['Material']])
 df['Temporada_Cod'] = encoder.fit_transform(df[['Temporada']])
+
+mapa_vendas = {
+    'Nenhum': 0,
+    '1': 1,
+    '2': 2,
+    '3': 3,
+    '4': 4,
+    '+5': 5,
+    '+25': 25,
+    '+50': 50,
+    '+100': 100,
+    '+1000': 1000,
+    '+10mil': 10000,
+    '+50mil': 50000
+}
+
+df['Qtd_Vendidos_Cod'] = df['Qtd_Vendidos'].map(mapa_vendas)
+
+marca_freq = df['Marca'].value_counts() / len(df) 
+df['Marca_Freq'] = df['Marca'].map(marca_freq)
+
+material_freq = df['Material'].value_counts() / len(df)
+df['Material_Freq'] = df['Material'].map(material_freq)
